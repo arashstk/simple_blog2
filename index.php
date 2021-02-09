7c9318544e9b275b78a61b4e06a0b6641acfe2ff
Arash <arash.nouri@chasacademy.se>
2021-02-09 18:10:12 +0100

    adding files first stage

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    include __DIR__ . "\app\bootstrap\autoload.php";
+   // require 'App\Controller\HomeController.php';
+    $homeController = new \App\Controller\HomeController();
+    var_dump($homeController);
+?>
