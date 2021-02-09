86568d4c2fc247871eac981f76f11570e70fd1a5
Arash <arash.nouri@chasacademy.se>
2021-02-09 21:01:36 +0100

    templates

---
+++ templates/header.php
@@ -0,0 +1,35 @@
+<?php
+     include __DIR__ . "\..\app\bootstrap\autoload.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Blog Home - Start Bootstrap Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="public/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/blog-home.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div class="btn-group" style="margin: 8px;float:right">
+    <a href="/login.php" class="btn btn-success sm">Login</a>
+    <a href="/register.php" class="btn btn-danger">Register</a>
+</div>
+
+
+</body>
+
+</html>
